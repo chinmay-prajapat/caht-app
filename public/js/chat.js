f7dbfc6727eaa6d03d55d4db38ac1b5cab0e0ccf
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-28 19:33:22 +0530

    broadcast message and disconnect the connection

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,7 +4,7 @@ const input = document
   .querySelector("#message-form")
   .addEventListener("submit", (e) => {
     e.preventDefault()
-    const message = document.querySelector("input").value
+    const message = e.target.elements.message.value
     socket.emit("sendMessage", message)
   })
 socket.on("message", (message) => {
