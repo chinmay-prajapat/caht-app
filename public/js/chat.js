038969a5dd393395ba2bf838e1d7906be232ba77
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-29 07:23:38 +0530

    bad word library implemented

--- public/js/chat.js
+++ public/js/chat.js
@@ -5,8 +5,27 @@ const input = document
   .addEventListener("submit", (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit("sendMessage", message)
+    socket.emit("sendMessage", message, (error) => {
+      if (error) {
+        return console.log(error)
+      }
+      console.log("The message was delivered!")
+    })
   })
 socket.on("message", (message) => {
   console.log(message)
+})
+
+document.querySelector("#send-location").addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser")
+  }
+
+  navigator.geolocation.getCurrentPosition(({ coords }) => {
+    const { latitude, longitude } = coords
+    socket.emit(
+      "sendLocation",
+      `https://google.com/maps?q=${latitude},${longitude}`,
+    )
+  })
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require("path")
 const http = require("http")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
 const express = require("express")
 
 const port = process.env.PORT || 3000
@@ -15,9 +16,20 @@ app.use(express.static(publicDirectoryPath))
 io.on("connection", (socket) => {
   socket.emit("message", "Welcome")
   socket.broadcast.emit("message", "A new user has joined")
-  socket.on("sendMessage", (message) => {
+
+  socket.on("sendMessage", (message, callback) => {
+    const filter = new Filter()
+    if (filter.isProfane(message)) {
+      return callback("Profanity is not allowed")
+    }
     io.emit("message", message)
+    callback()
   })
+
+  socket.on("sendLocation", (data) => {
+    io.emit("message", data)
+  })
+
   socket.on("disconnect", () => {
     io.emit("message", "A user has left")
   })
