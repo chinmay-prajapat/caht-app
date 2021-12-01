fedbb98d606e1788a8c3b9412958bbdf74b8f4df
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-01 18:56:05 +0530

    personal room has been added

--- src/index.js
+++ src/index.js
@@ -1,45 +1,60 @@
 const path = require("path")
 const http = require("http")
+const express = require("express")
 const socketio = require("socket.io")
 const Filter = require("bad-words")
-const express = require("express")
-const { generateMessage } = require("./utils/messages")
-const port = process.env.PORT || 3000
+const { generateMessage, generateLocationMessage } = require("./utils/messages")
 
 const app = express()
 const server = http.createServer(app)
 const io = socketio(server)
 
+const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, "../public")
+
 app.use(express.static(publicDirectoryPath))
 
 io.on("connection", (socket) => {
+  console.log("New WebSocket connection")
+
   socket.on("join", ({ username, room }) => {
     socket.join(room)
-    socket.emit("message", generateMessage("welcome!"))
+
+    socket.emit("message", generateMessage("Welcome!"))
     socket.broadcast
       .to(room)
-      .emit("message", generateMessage(`${username} has joined !`))
+      .emit("message", generateMessage(`${username} has joined!`))
+
+    // socket.emit, io.emit, socket.broadcast.emit
+    // io.to.emit, socket.broadcast.to.emit
   })
+
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter()
+
     if (filter.isProfane(message)) {
-      return callback("Profanity is not allowed")
+      return callback("Profanity is not allowed!")
     }
-    io.emit("message", generateMessage(message))
+
+    io.to("Center City").emit("message", generateMessage(message))
     callback()
   })
 
-  socket.on("sendLocation", (data, callback) => {
-    io.emit("locationMessage", generateMessage(data))
+  socket.on("sendLocation", (coords, callback) => {
+    io.emit(
+      "locationMessage",
+      generateLocationMessage(
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
+      ),
+    )
     callback()
   })
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("User has been disconnected"))
+    io.emit("message", generateMessage("A user has left!"))
   })
 })
 
 server.listen(port, () => {
-  console.log(`The server is running on ${port}`)
+  console.log(`Server is up on port ${port}!`)
 })
