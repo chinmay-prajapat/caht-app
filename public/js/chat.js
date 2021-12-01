850d949d92fde6037bb505be5a002d104dfe3c3d
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-01 07:06:52 +0530

    template added

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,6 +4,17 @@ const $messageForm = document.querySelector("#message-form")
 const $messageFormInput = $messageForm.querySelector("input")
 const $messageFormButton = $messageForm.querySelector("button")
 const $sendLocation = document.querySelector("#send-location")
+const $messages = document.querySelector("#messages")
+
+const messageTemplate = document.querySelector("#message-template").innerHTML
+
+socket.on("message", (message) => {
+  console.log(message)
+  const html = Mustache.render(messageTemplate, {
+    message,
+  })
+  $messages.insertAdjacentHTML("beforeend", html)
+})
 
 const input = document
   .querySelector("#message-form")
