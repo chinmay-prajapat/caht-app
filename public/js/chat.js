3eb0da811ff4dff6b51c50e6e9ee0e3d6280aab3
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-03 22:22:23 +0530

    removed console logs

--- public/js/chat.js
+++ public/js/chat.js
@@ -37,7 +37,6 @@ const autoscroll = () => {
 }
 
 socket.on("message", (message) => {
-  console.log(message)
   const html = Mustache.render(messageTemplate, {
     username: message.username,
     message: message.text,
@@ -48,7 +47,6 @@ socket.on("message", (message) => {
 })
 
 socket.on("locationMessage", (message) => {
-  console.log(message)
   const html = Mustache.render(locationMessageTemplate, {
     username: message.username,
     url: message.url,
