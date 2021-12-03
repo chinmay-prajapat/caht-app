511e6ccc655c44670ef924befdfc3b987cb1ca9e
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-12-03 18:49:21 +0530

    separate room for each user

--- src/index.js
+++ src/index.js
@@ -23,34 +23,43 @@ app.use(express.static(publicDirectoryPath))
 io.on("connection", (socket) => {
   console.log("New WebSocket connection")
 
-  socket.on("join", ({ username, room }) => {
-    addUser({ id: socket.id })
-    socket.join(room)
+  socket.on("join", ({ username, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, username, room })
+
+    if (error) {
+      return callback(error)
+    }
+
+    socket.join(user.room)
 
     socket.emit("message", generateMessage("Welcome!"))
     socket.broadcast
       .to(room)
       .emit("message", generateMessage(`${username} has joined!`))
+    callback()
 
     // socket.emit, io.emit, socket.broadcast.emit
     // io.to.emit, socket.broadcast.to.emit
   })
 
   socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id)
+
     const filter = new Filter()
 
     if (filter.isProfane(message)) {
       return callback("Profanity is not allowed!")
     }
 
-    io.to("Center City").emit("message", generateMessage(message))
+    io.to(user.room).emit("message", generateMessage(message))
     callback()
   })
 
   socket.on("sendLocation", (coords, callback) => {
-    io.emit(
+    const user = getUser(socket.id)
+    io.to(user.room).emit(
       "locationMessage",
-      generateLocationMessage(
+      generateMessage(
         `https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
       ),
     )
@@ -58,7 +67,14 @@ io.on("connection", (socket) => {
   })
 
   socket.on("disconnect", () => {
-    io.emit("message", generateMessage("A user has left!"))
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit(
+        "message",
+        generateMessage(`${user.username} has left!`),
+      )
+    }
   })
 })
 
